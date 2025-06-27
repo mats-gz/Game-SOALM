00f739dc8d8e90a202920844df7f93ccb26d4fc5
matiass :3 <matiasguzmanrodriguez89@gmail.com>
2025-06-27 12:06:53 -0300

    Juego 2

--- Scripts/Protagonista.gd
+++ Scripts/Protagonista.gd
@@ -4,13 +4,30 @@ extends CharacterBody2D
 const SPEED = 120.0
 const JUMP_VELOCITY = -260.0
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
-const ATAQUE_BASICO = preload("res://Escenas/Poderes/AtaqueBasico.tscn")
+const ATAQUE_BASICO = preload("res://Escenas/Poderes/BolaFuego.tscn")
+@onready var pos_disparo: Marker2D = $PosDisparo
+
+
+func _input(event):
+	
+#region Disparo con Click / Ataq. Básico
+	if event.is_action_pressed("primer_ataque"):
+		var bola = ATAQUE_BASICO.instantiate()
+		bola.position = pos_disparo.global_position
+		bola.direccion = Vector2.LEFT if animated_sprite.flip_h else Vector2.RIGHT
+		get_tree().current_scene.add_child(bola)
+#endregion
 
 func _physics_process(delta: float) -> void:
+#region Gravedad Juego
+
 	# Añadir gravedad al juego.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		
+#endregion
 
+#region Movimiento del Jugador
 	# Acción de saltar.
 	if Input.is_action_just_pressed("salto") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
@@ -38,5 +55,6 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		
 	move_and_slide()
+#endregion
--- Scripts/Slime.gd
+++ Scripts/Slime.gd
@@ -5,9 +5,13 @@ var direc = 1
 @onready var ray_cast_der: RayCast2D = $RayCastDer
 @onready var ray_cast_izq: RayCast2D = $RayCastIzq
 @onready var animacion_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var bola_fuego: Node2D = $"."
+var vida := 3
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+#region Direccion de movimiento del Slime
+
 	if ray_cast_der.is_colliding():
 		direc = -1
 		animacion_sprite.flip_h = true
@@ -16,3 +20,13 @@ func _process(delta):
 		animacion_sprite.flip_h = false
 		
 	position.x += delta * vel * direc
+	
+#endregion
+
+func recibir_dano(cantidad):
+	vida -= cantidad
+	if vida <= 0:
+		morir()
+
+func morir():
+	queue_free()  # O podés reproducir una animación y luego desaparecer
