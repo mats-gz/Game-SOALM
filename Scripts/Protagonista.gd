1b574ad66ac548eb16411bcbcb1b2d3cc1998e47
matiass :3 <matiasguzmanrodriguez89@gmail.com>
2025-06-26 23:12:18 -0300

    Organizacion

--- Scripts/Protagonista.gd
+++ Scripts/Protagonista.gd
@@ -4,7 +4,7 @@ extends CharacterBody2D
 const SPEED = 120.0
 const JUMP_VELOCITY = -260.0
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
-
+const ATAQUE_BASICO = preload("res://Escenas/Poderes/AtaqueBasico.tscn")
 
 func _physics_process(delta: float) -> void:
 	# Añadir gravedad al juego.
@@ -14,10 +14,10 @@ func _physics_process(delta: float) -> void:
 	# Acción de saltar.
 	if Input.is_action_just_pressed("salto") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+	
 	# Obtener la dirección de movimiento del jugador.
 	var direction := Input.get_axis("mover_der", "mover_izq")
-	
+
 	#Dar vuelta el sprite
 	if direction > 0:
 		animated_sprite.flip_h = false
